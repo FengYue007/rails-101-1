7edd00bbd876e6e38ff2edf2f4e7579d21badf01
FengYue <10761101@gm.nfu.edu.tw>
2022-01-06 15:37:34 +0800

    add edit & create & delete post

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,33 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to account_posts_path
+  end
+
+
 end
